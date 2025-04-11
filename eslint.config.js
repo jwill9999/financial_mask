34780cd87212a16aca564845848dc02084b823ef
Jason Williams <jasonwilliams2308@gmail.com>
2025-04-11 04:28:54 +0100

    add accessability linting and fix errors

--- eslint.config.js
+++ eslint.config.js
@@ -43,18 +43,7 @@ module.exports = tseslint.config(
       "jsx-a11y": jsxA11y,
     },
     rules: {
-      // Common accessibility rules
-      "@angular-eslint/template/accessibility-alt-text": "error",
-      "@angular-eslint/template/accessibility-elements-content": "error",
-      "@angular-eslint/template/accessibility-label-has-associated-control": "error",
-      "@angular-eslint/template/accessibility-table-scope": "error",
-      "@angular-eslint/template/accessibility-valid-aria": "error",
-      "@angular-eslint/template/click-events-have-key-events": "error",
-      "@angular-eslint/template/mouse-events-have-key-events": "error",
-      "@angular-eslint/template/no-autofocus": "error",
-      "@angular-eslint/template/no-positive-tabindex": "error",
-      
-      // JSX A11y rules that can work with Angular templates
+      // JSX A11y rules that work with Angular templates
       "jsx-a11y/aria-props": "error",
       "jsx-a11y/aria-proptypes": "error",
       "jsx-a11y/aria-unsupported-elements": "error",
